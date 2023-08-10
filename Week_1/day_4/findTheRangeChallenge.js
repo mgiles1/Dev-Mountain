95625b13d26cd2f5dcc7a31da2f623620fcf7112
Michael Giles <bdgiles2424@gmail.com>
2023-08-10 09:58:11 -0500

    adding files for the first time

---
+++ Week_1/day_4/findTheRangeChallenge.js
@@ -0,0 +1,16 @@
+let arr = [1,4,11,2,37,-4]
+
+let lowest = arr[0]
+let highest = arr[0]
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+    if(arr[i] < lowest){
+        lowest = arr[i]
+    }
+    if(arr[i] > highest || highest === undefined) {
+        highest = arr[i]
+    }
+}
+
+    console.log(`inside the array ${arr}, the lowest value is ${lowest} and the highest value is ${highest}`)
