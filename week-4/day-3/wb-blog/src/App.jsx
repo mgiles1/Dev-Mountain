af91a125d4e3f8a915a66307a7ca43349c7ea4f4
Michael Giles <bdgiles2424@gmail.com>
2023-08-30 11:04:19 -0500

    Adding files

---
+++ week-4/day-3/wb-blog/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Nav from './components/Nav/Nav.jsx';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Components/Home/Home.jsx'
+import TopicList from './Components/TopicList/TopicList.jsx'
+import Post from './Components/Post/Post.jsx'
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/topics' element={<TopicList />}/>
+        <Route path='/post/:id' element={<Post />}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
